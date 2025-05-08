d6a6c7797dbc80a5ad96b97d65cbc724b5af9569
aayyadevara02 <akasha5@illinois.edu>
2025-05-07 20:35:28 -0500

    Update bert_classifier.py

--- bert_classifier.py
+++ bert_classifier.py
@@ -8,7 +8,7 @@ import numpy as np
 from transformers import TrainingArguments
 from sklearn.preprocessing import LabelEncoder
 
-# Check for GPU
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 
